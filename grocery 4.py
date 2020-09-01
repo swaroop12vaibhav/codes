509389da89bc837f167927f7d53d95ec549c5787
swaroop12vaibhav <70277655+swaroop12vaibhav@users.noreply.github.com>
2020-09-01 18:18:58 +0530

    Add files via upload

---
+++ grocery 4.py
@@ -0,0 +1,8 @@
+pasta=120
+sauce=150
+garlic=130
+seasoning=90
+bread=200
+meatballs=450
+subtotal=(pasta+sauce+garlic+seasoning+bread+meatballs)
+print(subtotal)
---
+++ input67.py
@@ -0,0 +1,6 @@
+name=input ("my name is :-")
+print("my name is "+name+"" )
+
+sport=input("my fav sport is:-")
+movie=input("my fav movie is:- ")
+print("my fav sport is "+sport+",my fav movie is"+movie+"")
